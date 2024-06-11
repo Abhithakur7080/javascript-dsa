8c4cf898087a21ddeeab967cf7e8c6930104978a
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-06-11 15:21:44 +0530

    3 problems added

---
+++ Leetcode/34.Find_first_and_last_position_in_sorted_array.js
@@ -0,0 +1,27 @@
+// problem link: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (nums, target) {
+  let start = -1;
+  let end = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      if (start === -1) {
+        start = i;
+      } else {
+        end = i;
+      }
+    }
+  }
+  if (start !== -1 && end === -1) {
+    end = start;
+  }
+  return [start, end];
+};
+
+console.log(searchRange([5,7,7,8,8,10], 8));//[ 3, 4 ]
+console.log(searchRange([5,7,7,8,8,10], 6));//[ -1, -1 ]
+console.log(searchRange([], 0));//[ -1, -1 ]
---
+++ Leetcode/35.search_insert_position.js
@@ -0,0 +1,25 @@
+// problem link: https://leetcode.com/problems/search-insert-position/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if(target === nums[mid]){
+        return mid
+    } else if(nums[mid] > target) {
+        right = mid - 1;
+    } else {
+        left = mid + 1;
+    }
+  }
+  return left;
+};
+
+console.log(searchInsert([1,3,5,6], 5));//2
+console.log(searchInsert([1,3,5,6], 2));//1
+console.log(searchInsert([1,3,5,6], 7));//4
