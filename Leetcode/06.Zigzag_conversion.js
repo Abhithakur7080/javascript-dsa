b4a22f682822cfb42a205b70d5be6e818340d433
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-06-13 14:47:13 +0530

    3 leetcode problem solved

--- Leetcode/06.Zigzag_conversion.js
+++ Leetcode/06.Zigzag_conversion.js
@@ -8,7 +8,6 @@ var convert = function(s, numRows) {
     if (numRows === 1) return s;
 
     let rows = new Array(numRows).fill("");
-    console.log(rows);
     let currentRow = 0;
     let goingDown = false;
 
@@ -23,6 +22,17 @@ var convert = function(s, numRows) {
     return rows.join("");
 };
 
-console.log(convert("PAYPALISHIRING", 3));
-console.log(convert("PAYPALISHIRING", 4));
-console.log(convert("A", 1));
+console.log(convert("PAYPALISHIRING", 3));//PAHNAPLSIIGYIR
+/*
+P     A     H     N   ->  PAHN
+A  P  L  S  I  I  G   ->  APLSIIG
+Y     I     R         ->  YIR
+*/
+console.log(convert("PAYPALISHIRING", 4));//PINALSIGYAHRPI
+/*
+P        I        N  -> PIN
+A     L  S    I   G  -> ALSIG
+Y  A     H  R        -> YAHR
+P        I           -> PI
+*/
+console.log(convert("A", 1));//A
